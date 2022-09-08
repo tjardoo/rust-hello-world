fb8dc61c11827d344b814a21b332c2abc267299c
Tjardo <dev@tjardo.com>
2022-09-08 16:24:20 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-#[derive(Debug)]
-struct DivisionByZeroError;
+use std::fs::File;
+use std::io::{Error, Read};
+use std::path::PathBuf;
 
-fn safe_division(dividend: f64, divisor: f64) -> Result<f64, DivisionByZeroError> {
-    if divisor == 0.0 {
-        Err(DivisionByZeroError)
-    } else {
-        Ok(dividend / divisor)
-    }
+fn read_file_contents(path: PathBuf) -> Result<String, Error> {
+    let mut string = String::new();
+
+    let mut file: File = match File::open(path) {
+        Ok(file_handle) => file_handle,
+        Err(io_error) => return Err(io_error),
+    };
+
+    match file.read_to_string(&mut string) {
+        Ok(_) => (),
+        Err(io_error) => return Err(io_error),
+    };
+
+    return Ok(string)
 }
 
 fn main() {
-    println!("{:?}", safe_division(9.0, 3.0));
-    println!("{:?}", safe_division(4.0, 0.0));
-    println!("{:?}", safe_division(0.0, 2.0));
+    if read_file_contents(PathBuf::from("src/main.rs")).is_ok() {
+        println!("The program found the main file.");
+    }
+    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
+        println!("The program reported an error for the file that doesn't exist.");
+    }
 }
