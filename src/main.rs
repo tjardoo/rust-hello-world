ac7cffab63cb8998f2c89c7cc278119cc5a4d2e3
Tjardo <dev@tjardo.com>
2022-09-08 15:54:37 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,16 @@
-struct Person {
-    first: String,
-    middle: Option<String>,
-    last: String,
-}
+#[derive(Debug)]
+struct DivisionByZeroError;
 
-fn build_full_name(person: &Person) -> String {
-    let mut full_name = String::new();
-
-    full_name.push_str(&person.first);
-    full_name.push_str(" ");
-
-    if let Some(middle) = &person.middle {
-        full_name.push_str(&middle);
-        full_name.push_str(" ");
+fn safe_division(dividend: f64, divisor: f64) -> Result<f64, DivisionByZeroError> {
+    if divisor == 0.0 {
+        Err(DivisionByZeroError)
+    } else {
+        Ok(dividend / divisor)
     }
-
-    full_name.push_str(&person.last);
-    
-    return full_name;
 }
 
 fn main() {
-    let john = Person {
-        first: String::from("James"),
-        middle: Some(String::from("Oliver")),
-        last: String::from("Smith"),
-    };
-    assert_eq!(build_full_name(&john), "James Oliver Smith");
-
-    let alice = Person {
-        first: String::from("Alice"),
-        middle: None,
-        last: String::from("Stevens"),
-    };
-    assert_eq!(build_full_name(&alice), "Alice Stevens");
-
-    let bob = Person {
-        first: String::from("Robert"),
-        middle: Some(String::from("Murdock")),
-        last: String::from("Jones"),
-    };
-    assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+    println!("{:?}", safe_division(9.0, 3.0));
+    println!("{:?}", safe_division(4.0, 0.0));
+    println!("{:?}", safe_division(0.0, 2.0));
 }
