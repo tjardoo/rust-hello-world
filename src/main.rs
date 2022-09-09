1c8a9e3ba43aa489f70a6091bcf45cbd9d40d4ec
Tjardo <dev@tjardo.com>
2022-09-09 12:20:51 +0200

    Test cases inside submodule

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,26 @@ fn main() {
     println!("total: {}", add(50, 40));
 }
 
-#[test]
-fn add_works() {
-    assert_eq!(add(1, 2), 3);
-    assert_eq!(add(10, 12), 22);
-    assert_eq!(add(5, -2), 3);
-}
+#[cfg(test)]
+mod add_function_tests {
+    use super::*;
 
-#[test]
-#[should_panic]
-fn add_fails() {
-    assert_eq!(add(2, 2), 7);
-}
+    #[test]
+    fn add_works() {
+        assert_eq!(add(1, 2), 3);
+        assert_eq!(add(10, 12), 22);
+        assert_eq!(add(5, -2), 3);
+    }
 
-#[test]
-#[ignore = "not yet reviewed by the Q.A. team"]
-fn add_negatives() {
-    assert_eq!(add(-2, -2), -4)
+    #[test]
+    #[should_panic]
+    fn add_fails() {
+        assert_eq!(add(2, 2), 7);
+    }
+
+    #[test]
+    #[ignore]
+    fn add_negatives() {
+        assert_eq!(add(-2, -2), -4)
+    }
 }
