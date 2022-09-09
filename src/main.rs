99b57861bc753fd933af260fe974f0481d1f48e0
Tjardo <dev@tjardo.com>
2022-09-09 12:20:10 +0200

    Tests cases

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-mod text_processing {
-
-    pub mod letters {
-        pub fn count_letters(text: &str) -> usize {
-            text.chars().filter(|ref c| c.is_alphabetic()).count()
-        }
-    }
-
-    pub mod numbers {
-        pub fn count_numbers(text: &str) -> usize {
-           text.chars().filter(|ref c| c.is_numeric()).count()
-        }
-    }
-}
-
-fn count_letters_and_numbers(text: &str) -> (usize, usize) {
-    let number_of_letters = text_processing::letters::count_letters(text);
-    let number_of_numbers = text_processing::numbers::count_numbers(text);
-    (number_of_letters, number_of_numbers)
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 fn main() {
-    assert_eq!(count_letters_and_numbers("221B Baker Street"), (12, 3));
-    assert_eq!(count_letters_and_numbers("711 Maple Street"), (11, 3));
-    assert_eq!(count_letters_and_numbers("4 Privet Drive"), (11, 1));
+    println!("total: {}", add(50, 40));
+}
+
+#[test]
+fn add_works() {
+    assert_eq!(add(1, 2), 3);
+    assert_eq!(add(10, 12), 22);
+    assert_eq!(add(5, -2), 3);
+}
+
+#[test]
+#[should_panic]
+fn add_fails() {
+    assert_eq!(add(2, 2), 7);
+}
+
+#[test]
+#[ignore = "not yet reviewed by the Q.A. team"]
+fn add_negatives() {
+    assert_eq!(add(-2, -2), -4)
 }
