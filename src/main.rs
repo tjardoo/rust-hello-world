bbf10f63f1b182c396899f23adc752aa4b2be072
Tjardo <dev@tjardo.com>
2022-09-09 10:47:24 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
-use std::fs::File;
-use std::io::{Error, Read};
-use std::path::PathBuf;
+use std::vec;
 
-fn read_file_contents(path: PathBuf) -> Result<String, Error> {
-    let mut string = String::new();
+fn copy_and_return<'names>(vector: &'names mut Vec<String>, value: &'names str) -> &'names String{
+    vector.push(String::from(value));
 
-    let mut file: File = match File::open(path) {
-        Ok(file_handle) => file_handle,
-        Err(io_error) => return Err(io_error),
-    };
-
-    match file.read_to_string(&mut string) {
-        Ok(_) => (),
-        Err(io_error) => return Err(io_error),
-    };
-
-    return Ok(string)
+    return vector.get(vector.len() - 1).unwrap();
 }
 
 fn main() {
-    if read_file_contents(PathBuf::from("src/main.rs")).is_ok() {
-        println!("The program found the main file.");
-    }
-    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
-        println!("The program reported an error for the file that doesn't exist.");
-    }
+    let name1 = "Joe";
+    let name2 = "Chris";
+    let name3 = "Anne";
+
+    let mut names = Vec::new();
+
+    assert_eq!("Joe", copy_and_return(&mut names, &name1));
+    assert_eq!("Chris", copy_and_return(&mut names, &name2));
+    assert_eq!("Anne", copy_and_return(&mut names, &name3));
+
+    assert_eq!(
+        names,
+        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+    )
 }
