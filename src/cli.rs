7621c86000db91da81e0c86bb47d2f19478afb65
Tjardo <dev@tjardo.com>
2022-09-09 17:02:07 +0200

    Create CLI program

---
+++ src/cli.rs
@@ -0,0 +1,28 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    Add {
+        #[structopt()]
+        text: String,
+    },
+    Done {
+        #[structopt()]
+        position: usize,
+    },
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Hello Cargo",
+    about = "A command line todo app written in Rust"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
